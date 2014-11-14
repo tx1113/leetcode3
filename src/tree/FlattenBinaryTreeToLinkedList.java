8bd5c80d0720d0b81a205b2d95474804db155d64
Xu Tian <tianxustartup@gmail.com>
2014-11-13 22:59:58 -0800

    add flattern binary tree

--- src/tree/FlattenBinaryTreeToLinkedList.java
+++ src/tree/FlattenBinaryTreeToLinkedList.java
@@ -35,45 +35,19 @@ public class FlattenBinaryTreeToLinkedList {
 	
 	/*
 	 * iteration: idea
+	 * user the preOrder traversal. 
+	 * st.add(root)
+	 * while !st.isEmpty()
+	 *    get the st.top() as node
+	 *    push node.right into st   
+	 *    push node.left into st
+	 *    
+	 *    let node.right = it's left, which is the current top element of the stack. 
+	 *    of course, we need to check the !st.isEmpty()
 	 * 
 	 */
 	
-	
-	public static void flattenRec(TreeNode root){
-		
-	}
-	
-	
-	
-	
-	/*
-	 * Idea: like the preOrder traversal. from the left most child.
-	 *  
-	 * */
-	
-	
-
-	
-	
-	public static void test(){
-		TreeNode root = BinaryTreeTraversal.buildTree();
-		flatten(root);
-		printLinkedList(root);
-	}
-	
-	public static void printLinkedList(TreeNode root){
-		TreeNode node = root;
-		while(node != null){
-			//visit the node
-			System.out.print(node.value + " ");
-			node = node.right;
-		}
-		System.out.println();
-	}
-	
-	
-	
-	public static void flatten(TreeNode root){
+	public static void flattenIter(TreeNode root){
 		Stack<TreeNode> st = new Stack<TreeNode>();
 		st.push(root);
 		while(!st.isEmpty()){
@@ -89,6 +63,50 @@ public class FlattenBinaryTreeToLinkedList {
 				p.right = st.peek();
 			}
 		}
+	}
+
+	public static void test(){
+		TreeNode root = BinaryTreeTraversal.buildTree();
+		flattenIter(root);
+		printLinkedList(root);
+		System.out.println("\n--------------");
+		TreeNode root2 = BinaryTreeTraversal.buildTree();
+		flattenRec(root2);
+		printLinkedList(root2);
+	}
+	
+	public static void printLinkedList(TreeNode root){
+		TreeNode node = root;
+		while(node != null){
+			//visit the node
+			System.out.print(node.value + " ");
+			node = node.right;
+		}
+		System.out.println();
+	}
+	
+	
+	
+	
+	
+	public static void flattenRec(TreeNode node){
+		if(node == null)
+			return ;
+		
+		flattenRec(node.left);
+		flattenRec(node.right);
+		
+		//!!! here it is very important. 
+		if(node.left == null)
+			return ;
+		TreeNode p = node.left;
+		while(p.right != null){
+			p = p.right;
+			//get the right most element of left subtree
+		}
+		p.right = node.right;
+		node.right = node.left;
+		node.left = null;
 	}
 	
 	
