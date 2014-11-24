05f8c832f5c246c02b911e9be7e194e0cc3c0fba
Xu Tian <tianxustartup@gmail.com>
2014-11-23 17:27:38 -0800

    Add v2 for Binary Tree Vertical Traversal

--- src/tree/BinaryTreeTraversalVerticalOrder.java
+++ src/tree/BinaryTreeTraversalVerticalOrder.java
@@ -1,5 +1,15 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.omg.CORBA.SystemException;
+
 import basicDS.TreeNode;
 
 public class BinaryTreeTraversalVerticalOrder {
@@ -7,6 +17,8 @@ public class BinaryTreeTraversalVerticalOrder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		testV1();
+		testV2();
+		testV3();
 	}
 	
 	/*
@@ -52,6 +64,8 @@ public class BinaryTreeTraversalVerticalOrder {
 	 *          print node.val
 	 *      printVerticalLine(node.left, lineNo, hd - 1);
 	 *      printVerticalLine(node.right, lineNo, hd + 1);
+	 * 
+	 * 
 	 */ 
 	
 	/*
@@ -114,4 +128,114 @@ public class BinaryTreeTraversalVerticalOrder {
 		
 		binaryTreeTraversalVerticalOrder(root);
 	}
+	
+	/*
+	 * We use a hash map to optimize
+	 * 
+	 * Time: 
+	 * 	if we use a hashMap, we can access the key in O(1) time. But the key is unsorted. 
+	 * 		So, Sorting the key we need O(n log n). The final time complexity is O(n log n)
+	 *  Also, 
+	 *  if we use a treeMap, which is implemented by red-black tree. 
+	 *       Its key is sorted. but getKey, remove, update,etc, we need O(log n)
+	 *       So the total time is O(n log n)
+	 */
+	
+	public static void printVerticalLine2(TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+		getVerticalOrder2(root, 0, map);
+		
+		//print the map out.
+		ArrayList<Integer> keyArr = new ArrayList<Integer>();
+		for (Integer key: map.keySet()) {
+			keyArr.add(key);
+		}
+		
+		Collections.sort(keyArr);
+		System.out.println(keyArr);
+		for (int i=0; i< keyArr.size(); i++) {
+			int hd = keyArr.get(i);
+			ArrayList<Integer> line = map.get(hd);
+			for (Integer ele: line) {
+				System.out.print(ele + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void getVerticalOrder2(TreeNode node, int hd, 
+			HashMap<Integer, ArrayList<Integer>> map) {
+		if (node == null) {
+			return ;
+		}
+		//push the node value of this hd into 
+		if (map.containsKey(hd)) {
+			map.get(hd).add(node.value);
+		} else {
+			ArrayList<Integer> newLine = new ArrayList<Integer>();
+			newLine.add(node.value);
+			map.put(hd, newLine);
+		}
+		getVerticalOrder2(node.left, hd - 1, map);
+		getVerticalOrder2(node.right, hd + 1, map);
+		
+	}
+	
+	public static void testV2() {
+		TreeNode root = BuildBinaryTree.buildTree();
+		System.out.println(BinaryTreeTraversal.preOrderRec(root));
+		System.out.println(BinaryTreeTraversal.inOrderRec(root));
+		System.out.println(BinaryTreeTraversal.postOrderRec(root));
+		printVerticalLine2(root);
+	}
+	
+	//treeMap
+	public static void getVerticalOrder3(TreeNode node, int hd, TreeMap<Integer, 
+			ArrayList<Integer>> treeMap) {
+		if (node == null) {
+			return ;
+		}
+		
+		ArrayList<Integer> curLine = null;
+		if (!treeMap.containsKey(hd)) {
+			curLine = new ArrayList<Integer>();
+			treeMap.put(hd, curLine);
+		}
+		curLine = treeMap.get(hd);
+		curLine.add(node.value);
+		
+		getVerticalOrder3(node.left, hd - 1, treeMap);
+		getVerticalOrder3(node.right, hd + 1, treeMap);
+		
+	}
+	
+	public static void printVerticalLine3(TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		TreeMap<Integer, ArrayList<Integer>> treeMap = new TreeMap<Integer, ArrayList<Integer>>();
+		getVerticalOrder3(root, 0, treeMap);
+		
+		//i must be a continuous Integer number from min to max
+		for (int i =treeMap.firstKey(); i <= treeMap.lastKey(); i++) {
+			ArrayList<Integer> line = treeMap.get(i);
+			
+			for(Integer j: line) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void testV3() {
+		TreeNode root = BuildBinaryTree.buildTree();
+		System.out.println(BinaryTreeTraversal.preOrderRec(root));
+		System.out.println(BinaryTreeTraversal.inOrderRec(root));
+		System.out.println(BinaryTreeTraversal.postOrderRec(root));
+		printVerticalLine3(root);
+	}
+	
 }
